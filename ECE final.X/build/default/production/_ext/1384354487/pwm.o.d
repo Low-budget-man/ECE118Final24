9eff3f84881eaeb2af8d3d9b3d23cafb25c0fdc6
Low-budget-man <121657723+Low-budget-man@users.noreply.github.com>
2024-05-09 16:44:03 -0700

    Im flip flopping hard with mplab x files

--- ECE final.X/build/default/production/_ext/1384354487/pwm.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/1384354487/pwm.o: \
- C:/Users/coope/OneDrive/Documents/UCSC/Classes/UCSC\ 3nd\ quarter\ classes\ y3/ECE\ 118/FinalProject/ece118-final/src/Provided/pwm.c \
- ../include/Provided/BOARD.h ../include/Provided/pwm.h
-
-../include/Provided/BOARD.h:
-
-../include/Provided/pwm.h:
--- ECE final.X/build/default/production/_ext/1384354487/timers.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/1384354487/timers.o: \
- C:/Users/coope/OneDrive/Documents/UCSC/Classes/UCSC\ 3nd\ quarter\ classes\ y3/ECE\ 118/FinalProject/ece118-final/src/Provided/timers.c \
- ../include/Provided/BOARD.h ../include/Provided/timers.h
-
-../include/Provided/BOARD.h:
-
-../include/Provided/timers.h:
